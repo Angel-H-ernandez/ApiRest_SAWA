c576cb1a241b5d906268fc0071415badf396d288
MiguelAngel <aldkl@jdjd.com>
2024-09-11 11:37:57 -0600

    ws permisos_plan add

--- app/Models/Permisos_plan_model.php
+++ app/Models/Permisos_plan_model.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permisos_plan_model extends Model
 {
+    public $timestamps = false;
     use HasFactory;
+    protected $table = "permisos_plan";
+    protected $fillable = [
+        'id',
+        'online',
+        'multiusuario',
+        'multisucursal',
+        'visualizar',
+        'vender',
+        'comprar'
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users_controller;
 use App\Http\Controllers\User_administrador_controller;
+use App\Http\Controllers\Permisos_plan_controller;
 
 Route::get('/version', function(){
     return "Version de api 1.0";
@@ -17,3 +18,7 @@ Route::get('/get-user/{id}', [Users_controller::class, 'show']);
 
 //LOGIN___________________________________________________________
 Route::post('/login', [User_administrador_controller::class, 'login']);
+
+//PERMISOS_PLAN______________________________________________________________
+Route::get('/show-permisos-plan/{id}', [Permisos_plan_controller::class, 'show']);
+Route::put('/update-permisos-plan/{id}', [Permisos_plan_controller::class, 'update']);
