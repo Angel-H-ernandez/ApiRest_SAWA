2581665b8b099d5a5f61fa8c09a0b3efd29dee52
MiguelAngel <aldkl@jdjd.com>
2024-09-11 11:09:46 -0600

    login ws add

---
+++ app/Http/Controllers/Permisos_plan_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Permisos_plan_controller extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users_controller;
+use App\Http\Controllers\User_administrador_controller;
 
 Route::get('/version', function(){
     return "Version de api 1.0";
@@ -13,3 +14,6 @@ Route::post('/create-user', [Users_controller::class, 'store']);
 Route::put('/update-user/{id}', [Users_controller::class, 'update']);
 Route::delete('/delete-user/{id}', [Users_controller::class, 'destroy']);
 Route::get('/get-user/{id}', [Users_controller::class, 'show']);
+
+//LOGIN___________________________________________________________
+Route::post('/login', [User_administrador_controller::class, 'login']);
